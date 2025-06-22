53db7b465fe7c328adbf85fb6758efd039783884
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-06-22 10:56:16 +0600

    Create Area of a Rectangular.js

---
+++ Code/Area of a Rectangular.js
@@ -0,0 +1,22 @@
+
+//Add Link 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initialscale=
+1.0">
+<title>Document</title>
+</head>
+<body>
+<script src = "index.js"></script>
+</body>
+</html>
+
+
+// Add Link 
+
+var length = parseInt (prompt("Enter Length : "));
+var width = parseInt(prompt("Enter Width : " ));
+var area = length * width;
+document.write("Area of a Rectangular = "+area);
