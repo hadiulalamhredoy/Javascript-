6c189b56321b0e3786515bafbf72b8f579ce2432
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-06-22 10:54:32 +0600

    Create Area of a Triangle .js

---
+++ Code/Area of a Triangle .js
@@ -0,0 +1,19 @@
+// Main Link 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initialscale=
+1.0">
+<title>Document</title>
+</head>
+<body>
+<script src = "index.js"></script>
+</body>
+</html>
+
+// Add Link  
+
+var base = parseFloat (prompt("Enter Base : "));
+var height = parseFloat(prompt("Enter Heigth : " ));
+var area = (base * height) / 2 ;
