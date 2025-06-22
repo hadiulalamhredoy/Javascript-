051a3627049af7e58221770456f8a5cc9d02e7fa
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-06-22 20:48:32 +0600

    Create Array.js

---
+++ Code/Array.js
@@ -0,0 +1,30 @@
+//Main Code :
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=\, initial-scale=1.0">
+<title>Document</title>
+</head>
+<body>
+<script src= "index.js"></script>
+</body>
+</html>
+
+//Link Code :
+var name1 , name2, name3, name4, name5;
+var name1 = "Samira";
+var name2 = "Nafisa";
+var name3 = "Anika";
+var name4 = "Lubaba";
+var name5 = "Zafir";
+var names = ["Samira","Nafisa","Anika","Lubaba","Zafir"];
+console.log(names.length);
+names.push("Wasi");
+names.push("Sami");
+names.push("Rafia");
+names.pop();
+console.log(names);
+names.pop();
+console.log(names);
+
