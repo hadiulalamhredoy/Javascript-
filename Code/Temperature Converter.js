18a831e3e0942b7a542db6d0afc6429bd7ae8115
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-06-22 11:02:37 +0600

    Create Temperature Converter.js

---
+++ Code/Temperature Converter.js
@@ -0,0 +1,43 @@
+//Convert Farenheit From Celcuis 
+//Main Link :
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initialscale=
+1.0">
+<title>Document</title>
+</head>
+<body>
+<script src = "index.js"></script>
+</body>
+</html>
+
+//Link Link:
+var farn = parseFloat (prompt("Enter Farenheit : "));
+var cels = (farn-32)/5 ;
+document.write("Celcuis = "+ cels);
+
+
+
+// Convert Celcuis From Farenheit 
+
+// Main Link:
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initialscale=
+1.0">
+<title>Document</title>
+</head>
+<body>
+<script src = "index.js"></script>
+</body>
+</html>
+
+//Add Link:
+
+ar cels = parseFloat (prompt("Enter Celcuis : "));
+var farn = (cels*(9/5)) + 32;
+document.write("Farenheit = "+ farn);
