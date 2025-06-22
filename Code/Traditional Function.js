167e41d910b04b814511a5ce40378ce679be3c85
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-06-22 20:46:20 +0600

    Create Traditional Function.js

---
+++ Code/Traditional Function.js
@@ -0,0 +1,41 @@
+//Main Code :
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=\, initial-scale=1.0">
+<title>Document</title>
+</head>
+<body>
+<script src= "./ternaryOperator.js"></script>
+</body>
+</html>
+
+//Link Code :
+// add function
+function addition (x,y) {
+var result = x + y;
+document.write("Sum = " + result + "<br>");
+}
+function subtraction (x,y) {
+var result = x - y;
+document.write("Sub = " + result + "<br>");
+}
+function subtraction (x,y) {
+var result = x - y;
+document.write("Sub = " + result + "<br>");
+
+}
+function multipication (x,y) {
+var result = x * y;
+document.write("Mul = " + result + "<br>");
+}
+function division (x,y) {
+var result = x / y;
+document.write("div = " + result + "<br>");
+}
+addition(10,20);
+subtraction(40,30);
+multipication(23,3);
+division(12,4);
+
