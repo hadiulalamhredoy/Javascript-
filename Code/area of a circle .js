94892d38db66f6a00f064b1dc9d05098f5d512a6
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-06-22 10:57:49 +0600

    Create area of a circle .js

---
+++ Code/area of a circle .js
@@ -0,0 +1,19 @@
+//Main Link :
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initialscale=
+1.0">
+<title>Document</title>
+</head>
+<body>
+<script src = "index.js"></script>
+</body>
+</html>
+
+//Add Link : 
+
+var radius = parseFloat (prompt("Enter Radius : "));
+var area = 3.1416 * radius * radius ;
+document.write("Area of a Circle = "+area);
