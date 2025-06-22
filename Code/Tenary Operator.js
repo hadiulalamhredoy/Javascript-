cfa8df588cbd7195ba09a800d252edc408542d75
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-06-22 20:44:09 +0600

    Create Tenary Operator.js

---
+++ Code/Tenary Operator.js
@@ -0,0 +1,23 @@
+//Main Code :
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=\, initial-scale=1.0">
+<title>Document</title>
+</head>
+<body>
+<script src= "./ternaryOperator.js"></script>
+</body>
+</html>
+
+//Link Code :
+var number= Number(prompt("Enter a number : "));
+if(number>0)
+{
+console.log("Positive");
+}
+else
+{
+console.log("Negative");
+}
